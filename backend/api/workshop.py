144cd8229555e87c07787938081dd6a1bf403496
Andy Dong <89438047+aandime@users.noreply.github.com>
2023-03-31 12:26:58 -0400

    Update workshop.py

--- backend/api/workshop.py
+++ backend/api/workshop.py
@@ -7,8 +7,8 @@ from .authentication import registered_user
 api = APIRouter(prefix="/api/workshop")
 
 @api.get("", response_model= Workshop, tags=["Workshop"])
-def get(title : str, user_svc: WorkshopService = Depends()):
-    return user_svc.get(title)
+def get(title : str, workshop_service: WorkshopService = Depends()):
+    return workshop_service.get(title)
 
 
 @api.post("", response_model=Workshop, tags = ["Workshop"])
