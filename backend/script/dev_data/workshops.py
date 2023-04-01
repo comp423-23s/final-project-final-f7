1da4a4c3be47b0ec5be4de2317cd4978ab8c3059
Ryan Sabo <ryansabo2020@gmail.com>
2023-04-01 19:12:54 -0400

    Create default Workshops in database and add them automatically

---
+++ backend/script/dev_data/workshops.py
@@ -0,0 +1,15 @@
+# Creates default Workshops in the database
+
+from ...models import User, Workshop
+
+
+app = Workshop(id=1, title="App Lab", description="Make apps", host_first_name="Ryan", host_last_name="Sabo", host_description="Ryan Sabo makes apps", location="Murphey 116", time="4/22/2023 - 9:00AM", requirements="Laptop", spots="20")
+
+game = Workshop(id=2, title="Game Dev", description="Make games", host_first_name="Matthew", host_last_name="Reddy", host_description="Matthew Reddy makes games", location="Phillips 215", time="4/3/2023 - 11:00AM", requirements="Laptop", spots="25")
+
+
+
+models = [
+    app,
+    game
+]
