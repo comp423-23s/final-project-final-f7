84d735755586d1e1732ee3b3b8df4b9f87363ae4
Ryan Sabo <ryansabo2020@gmail.com>
2023-04-12 19:21:15 -0400

    Add test for create workshops

--- backend/test/services/workshop_test.py
+++ backend/test/services/workshop_test.py
@@ -2,14 +2,16 @@
 
 
 import pytest
-from ..models import Workshop
-from ..services import WorkshopService
-from ..entities import WorkshopEntity
+from ...models import Workshop
+from ...services import WorkshopService
+from ...entities import WorkshopEntity
 from sqlalchemy.orm import Session
 
 # Mock Workshops
 app = Workshop(id=1, title="App Lab", description="Make apps", host_first_name="Ryan", host_last_name="Sabo", host_description="Ryan Sabo makes apps", location="Murphey 116", time="4/22/2023 - 9:00AM", requirements="Laptop", spots="20")
 game = Workshop(id=2, title="Game Dev", description="Make games", host_first_name="Matthew", host_last_name="Reddy", host_description="Matthew Reddy makes games", location="Phillips 215", time="4/3/2023 - 11:00AM", requirements="Laptop", spots="25")
+csxl = Workshop(id=3, title="CSXL Training", description="Train for CSXL", host_first_name="Kris", host_last_name="Jordan", host_description="Kris Jordan trains TAs", location="Sitterson 156", time="4/12/2023 - 3:00PM", requirements="Laptop", spots="10")
+
 
 
 @pytest.fixture(autouse=True)
@@ -19,10 +21,12 @@ def setup_teardown(test_session: Session):
     test_session.add(app_workshop_entity)
     game_workshop_entity = WorkshopEntity.from_model(game)
     test_session.add(game_workshop_entity)
-
     test_session.commit()
-
     yield
+
+@pytest.fixture()
+def workshop(test_session: Session):
+    return WorkshopService(test_session)
 
 # Test to see all added titles
 def test_get_all_titles(workshop: WorkshopService):
@@ -36,4 +40,15 @@ def test_get_all_description(workshop: WorkshopService):
     workshop_all = workshop.get_all() 
     assert workshop_all[0].description == "Make apps"
     assert workshop_all[1].description == "Make games"
+    
+# Test to create new workshop and get title and description
+def test_create_workshop(workshop: WorkshopService):
+    workshop.create(csxl)
+    workshop_all = workshop.get_all() 
+    assert workshop_all[0].title == "App Lab"
+    assert workshop_all[1].title == "Game Dev"
+    assert workshop_all[2].title == "CSXL Training"
+    assert workshop_all[0].description == "Make apps"
+    assert workshop_all[1].description == "Make games"
+    assert workshop_all[2].description == "Train for CSXL"
     
