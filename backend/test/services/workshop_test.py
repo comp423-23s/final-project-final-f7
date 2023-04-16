82b5e5eff3485f808e3ebe79869957fe30b02e49
Ryan Sabo <ryansabo2020@gmail.com>
2023-04-16 15:07:35 -0400

    Build test framework and mock for register users, WIP

--- backend/test/services/workshop_test.py
+++ backend/test/services/workshop_test.py
@@ -2,9 +2,9 @@
 
 
 import pytest
-from ...models import Workshop
-from ...services import WorkshopService
-from ...entities import WorkshopEntity
+from ...models import Workshop, User, Permission
+from ...services import WorkshopService, PermissionService
+from ...entities import WorkshopEntity, PermissionEntity
 from sqlalchemy.orm import Session
 
 # Mock Workshops
@@ -13,11 +13,17 @@ game = Workshop(id=2, title="Game Dev", description="Make games", host_first_nam
 csxl = Workshop(id=3, title="CSXL Training", description="Train for CSXL", host_first_name="Kris", host_last_name="Jordan", host_description="Kris Jordan trains TAs", location="Sitterson 156", time="4/12/2023 - 3:00PM", requirements="Laptop", spots="10")
 register = Workshop(id=4, title="Registration Training", description="Train for registration", host_first_name="Ketan", host_last_name="Mayer-Patel", host_description="KMP trains students to register", location="Sitterson 014", time="4/13/2023 - 4:00PM", requirements="Laptop", spots="30")
 
+# Mock Permissions
+student_permission: Permission
+
+# Mock Users
+ryan = User(id=1, pid=123456789, onyen="ryansabo", first_name="Ryan", last_name="Sabo", email="ryansabo@test.com", pronouns="he/him", permissions=student_permission)
+matthew = User(id=2, pid=987654321, onyen="matthewreddy", first_name="Matthew", last_name="Reddy", email="matthewreddy@test.com", pronouns="he/him", permissions=student_permission)
 
 
 @pytest.fixture(autouse=True)
 def setup_teardown(test_session: Session):
-    # Bootstrap app and game
+    # Bootstrap app and game mock workshops
     app_workshop_entity = WorkshopEntity.from_model(app)
     test_session.add(app_workshop_entity)
     game_workshop_entity = WorkshopEntity.from_model(game)
@@ -29,20 +35,20 @@ def setup_teardown(test_session: Session):
 def workshop(test_session: Session):
     return WorkshopService(test_session)
 
-# Test to see all added titles
+# Test to see all titles for added workshops
 def test_get_all_titles(workshop: WorkshopService):
     workshop_all = workshop.get_all() 
     assert len(workshop_all) == 2
     assert workshop_all[0].title == "App Lab"
     assert workshop_all[1].title == "Game Dev"
 
-# Test to see all added descriptions
+# Test to see all descriptions for added workshops
 def test_get_all_description(workshop: WorkshopService):
     workshop_all = workshop.get_all() 
     assert workshop_all[0].description == "Make apps"
     assert workshop_all[1].description == "Make games"
     
-# Test to create new workshop and get title and description
+# Test to create a new workshop and get its title and description
 def test_create_workshop(workshop: WorkshopService):
     workshop.create(csxl)
     workshop_all = workshop.get_all() 
@@ -53,7 +59,7 @@ def test_create_workshop(workshop: WorkshopService):
     assert workshop_all[1].description == "Make games"
     assert workshop_all[2].description == "Train for CSXL"
     
-# Test to delete workshop
+# Test to delete a workshop
 def test_delete_workshop(workshop: WorkshopService):
     workshop.create(csxl)
     workshop.delete(csxl.id)
@@ -65,3 +71,7 @@ def test_delete_workshop(workshop: WorkshopService):
     assert workshop_all[0].description == "Make apps"
     assert workshop_all[1].description == "Make games"
     assert workshop_all[2].description == "Train for registration"
+
+# Test to register users
+def test_register_users(workshop: WorkshopService):
+    assert 1 == 1
